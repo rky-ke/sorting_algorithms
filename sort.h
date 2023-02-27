b7ae44ef5f42c9243f43602c168848496231a6bd
rky-ke <ronaldyegon15@gmail.com>
2023-02-27 04:44:49 -0800

    Selection Sort

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void print_array(const int *array, size_t size);
 listint_t *swap_node(listint_t *node, listint_t **list);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
 
